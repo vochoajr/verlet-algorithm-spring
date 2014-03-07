6aa859e40ac293ac0faa42d819f57f1305dd4cc9
Vicente Ochoa <vo283@nyu.edu>
2014-03-07 00:42:49 -0500

    Addition of the damping term to the force

--- Oscillations.py
+++ Oscillations.py
@@ -17,14 +17,14 @@ tlist, rlist, vlist = [], [], []
 for i in range(100):
 	v = v + dt * F / m #update v
 	r = r + dt * v     #update r
-	F = -2*r           #recalculate force
+	F = -2*r + -0.1*v  #recalculate force, with damping
 	v = v + dt * F / m #update v
 	tlist.append(i)    #add time to list
 	rlist.append(r)    #add position to list
 	vlist.append(v)    #add velocity to list
 
 #setup figures
-fig = plt.figure(figsize=(13,5)) #figsize=(width, height) in inches
+fig = plt.figure(figsize=(13,5)) #figsize=(width, height)
 fig_1 = fig.add_subplot(1,2,1)
 fig_2 = fig.add_subplot(1,2,2)
 
